86892789308dae7d5e051b3358ac7059a4b5ad57
HCHcompany <51677708+HCHcompany@users.noreply.github.com>
2023-10-12 16:29:14 -0300

    update

--- app/src/main/java/com/launcher/anc/MainActivity.java
+++ app/src/main/java/com/launcher/anc/MainActivity.java
@@ -1,13 +1,13 @@
 package com.launcher.anc;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home);
     }
 }
