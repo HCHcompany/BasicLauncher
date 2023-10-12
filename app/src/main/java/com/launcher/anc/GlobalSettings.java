d5ab3948b51eff213de164d3833c38e3dcd76bfd
HCHcompany <51677708+HCHcompany@users.noreply.github.com>
2023-10-12 19:35:05 -0300

    update

--- app/src/main/java/com/launcher/anc/GlobalSettings.java
+++ app/src/main/java/com/launcher/anc/GlobalSettings.java
@@ -2,14 +2,7 @@ package com.launcher.anc;
 
 public class GlobalSettings{
     //Global
-    public static final String EMPTY_MESSAGE_DEFAULT_GRIDVIEW = "No existen aplicaciones instaladas.";
-
-
-    //com.launcher.anc.home.GridFragment
-    public static final int INTERNAL_EMPTY_ID = 0x00ff0001; // ID para texto que se muestra por defecto.
-    public static final int INTERNAL_PROGRESS_CONTAINER_ID = 0x00ff0002; // ID para barra de progreso.
-    public static final int INTERNAL_LIST_CONTAINER_ID = 0x00ff0003; // ID para la grilla con aplicaciones.
-
+    public static final String EMPTY_MESSAGE_DEFAULT_GRIDVIEW = "No existen aplicaciones instaladas."; //Mensaje general de vacio.
     public static final int WIDTH_COLUMS_GRID_APPS = 60; // 60, 70 o adaptativo.
     public static final int NUMS_COLUMS_GRID_APPS = 2; //2 o se trabajara en otra opcion (adaptativo = GridView.AUTO_FIT).
     public static final int SPACES_HORIZONTAL_GRID_APPS = 4; // Espacio por cada item de manera horizontal.
