b0fff69651d461e2b19367d4d09ff060005a4f61
HCHcompany <51677708+HCHcompany@users.noreply.github.com>
2023-10-12 18:52:07 -0300

    update

--- app/src/main/java/com/launcher/anc/model/AppLoader.java
+++ app/src/main/java/com/launcher/anc/model/AppLoader.java
@@ -1,4 +1,64 @@
 package com.launcher.anc.model;
 
-public class AppLoader {
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.loader.content.AsyncTaskLoader;
+
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class AppLoader extends AsyncTaskLoader<ArrayList<AppModel>> {
+
+    private final Context context;
+    private ArrayList<AppModel> installedApps;
+    private final PackageManager pm;
+    private PackageReceiver receiver;
+    public static final Comparator<AppModel> ALPHA_COMPARATOR = new Comparator<AppModel>() {
+        private final Collator collator = Collator.getInstance();
+        @Override
+        public int compare(AppModel appModel, AppModel t1) {
+            return collator.compare(appModel.getLabel(), t1.getLabel());
+        }
+    };
+
+    public AppLoader(Context context) {
+        super(context);
+        this.context = context;
+        this.pm = this.context.getPackageManager();
+    }
+
+    @Nullable
+    @Override
+    public ArrayList<AppModel> loadInBackground() {
+        return null;
+    }
+
+    @Override
+    public void deliverResult(@Nullable ArrayList<AppModel> data) {
+        super.deliverResult(data);
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+    }
+
+    @Override
+    protected void onStopLoading() {
+        super.onStopLoading();
+    }
+
+    @Override
+    public void onCanceled(@Nullable ArrayList<AppModel> data) {
+        super.onCanceled(data);
+    }
+
+    @Override
+    protected void onReset() {
+        super.onReset();
+    }
 }
--- app/src/main/java/com/launcher/anc/model/PackageReceiver.java
+++ app/src/main/java/com/launcher/anc/model/PackageReceiver.java
@@ -1,4 +1,38 @@
 package com.launcher.anc.model;
 
-public class PackageReceiver {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
+/**
+ * Esta clase esta dirigida a recibir informacion de las aplicaciones por medio de filtros
+ * Sirve para actualizar informacion de nuevas aplicaciones y de las que son removidas
+ * Ademas sirve para validar si una aplicacion se encuentra disponible, esta opcion es por si se encuentra
+ * instalada en la tarjeta SD.*/
+public class PackageReceiver extends BroadcastReceiver{
+
+    private final AppLoader loader; //Cargador de apllicaciones.
+
+    public PackageReceiver(AppLoader loader){
+        this.loader = loader;
+
+        //Filtro para recibir actualizaciones de aplicaciones removidas o actualizadas.
+        IntentFilter filter = new IntentFilter(Intent.ACTION_PACKAGE_ADDED);
+        filter.addAction(Intent.ACTION_PACKAGE_REMOVED);
+        filter.addAction(Intent.ACTION_PACKAGE_CHANGED);
+        filter.addDataScheme("package");
+        this.loader.getContext().registerReceiver(this, filter);
+
+        //Regístrar eventos relacionados con la instalación de apps en la tarjeta SD.
+        IntentFilter sdFilter = new IntentFilter();
+        sdFilter.addAction(Intent.ACTION_EXTERNAL_APPLICATIONS_AVAILABLE);
+        sdFilter.addAction(Intent.ACTION_EXTERNAL_APPLICATIONS_UNAVAILABLE);
+        this.loader.getContext().registerReceiver(this, sdFilter);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent){
+        this.loader.onContentChanged();
+    }
 }
