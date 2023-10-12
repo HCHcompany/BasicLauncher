2333b1f0eb41bc4f7356f39888c035bfa480f28f
HCHcompany <51677708+HCHcompany@users.noreply.github.com>
2023-10-12 19:33:44 -0300

    update

--- app/src/main/java/com/launcher/anc/GlobalSettings.java
+++ app/src/main/java/com/launcher/anc/GlobalSettings.java
@@ -11,7 +11,7 @@ public class GlobalSettings{
     public static final int INTERNAL_LIST_CONTAINER_ID = 0x00ff0003; // ID para la grilla con aplicaciones.
 
     public static final int WIDTH_COLUMS_GRID_APPS = 60; // 60, 70 o adaptativo.
-    public static final int NUMS_COLUMS_GRID_APPS = 3; //2 o se trabajara en otra opcion (adaptativo = GridView.AUTO_FIT).
+    public static final int NUMS_COLUMS_GRID_APPS = 2; //2 o se trabajara en otra opcion (adaptativo = GridView.AUTO_FIT).
     public static final int SPACES_HORIZONTAL_GRID_APPS = 4; // Espacio por cada item de manera horizontal.
     public static final int SPACES_VERTICAL_GRID_APPS = 4; // Espacio por cada item de manera vertical.
 
--- app/src/main/java/com/launcher/anc/old/home/HomeGridFragment.java
+++
@@ -1,5 +0,0 @@
-package com.launcher.anc.old.home;
-
-public class HomeGridFragment extends HomeGridFragmentBase {
-
-}
