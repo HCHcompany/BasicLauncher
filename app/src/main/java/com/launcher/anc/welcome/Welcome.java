9dca4fba16cfd98e8fd0cd096c8313d735a6ebac
HCHcompany <51677708+HCHcompany@users.noreply.github.com>
2023-10-15 14:35:20 -0300

    update

--- app/src/main/java/com/launcher/anc/welcome/Welcome.java
+++ app/src/main/java/com/launcher/anc/welcome/Welcome.java
@@ -1,4 +1,35 @@
 package com.launcher.anc.welcome;
 
-public class Welcome {
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Welcome extends Fragment {
+
+    private Context context;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        context = getActivity();
+        FrameLayout frame = new FrameLayout(context);
+        frame.setFitsSystemWindows(true);
+
+        //Bienvenida.
+        LinearLayout welcome = new LinearLayout(frame.getContext());
+
+        //Preferencias
+
+        //Tour
+
+        return frame;
+    }
 }
---
+++ app/src/main/java/com/launcher/anc/welcome/WelcomeView.java
@@ -0,0 +1,8 @@
+package com.launcher.anc.welcome;
+
+import androidx.fragment.app.FragmentActivity;
+
+public class WelcomeView extends FragmentActivity {
+
+
+}
